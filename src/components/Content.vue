1ae1dfbedd1fd9634e03256143ce9b4e4bc635cc
Rob O'Callaghan <rob@autto.io>
2019-10-04 17:39:27 +0100

    Added Child Components

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -5,15 +5,26 @@
       <li v-for="child in childComponents">{{ child }}</li>
     </ul>
     {{ weather_data }}
+    <temp-var-chart :tempVar="tempVar"></temp-var-chart>
+    <today-highlights :highlights="highlights"></today-highlights>
   </div>
 </template>
 
 <script>
+import TempVarChart from './TempVarChart.vue'
+import Highlights from './Highlights.vue'
+
 export default {
   props: ["weather_data"],
+  components: {
+    'temp-var-chart': TempVarChart,
+    'today-highlights': Highlights
+  },
   data () {
     return {
-      childComponents: ['TempVarChart.vue', 'Highlights.vue']
+      childComponents: ['TempVarChart.vue', 'Highlights.vue'],
+      tempVar: this.weather_data.temperature,
+      highlights: this.weather_data.highlights,
     }
   },
   methods: {
@@ -26,8 +37,5 @@ export default {
 </script>
 
 <style>
-#pagecontent {
-  border: 1px solid black;
-  padding: 2px;
-}
+
 </style>
